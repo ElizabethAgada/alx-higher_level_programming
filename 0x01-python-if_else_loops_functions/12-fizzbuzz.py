d193cf7ab8ddacc8d115bca0cbe60feb093d9cc6
Elizabeth Agada <lizzyegbo2015@gmail.com>
2023-11-29 08:28:58 -0800

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+# Author - ElizabethAgada
+"""print the numbers from 1 to 100 seperated by a space
+    For multiples of three, print Fizz instead of the number
+    For multiples of five, print Buzz instead of the number
+    for multiples of three and five, print FizzBuzz instead of the number.
+    """
+
+
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="")
-        elif i % 3 == 0:
-            print("Fizz", end="")
-        elif i % 5 == 0:
-            print("Buzz", end="")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{:d} ".format(i), end="")
+            print("{} ".format(number), end="")
