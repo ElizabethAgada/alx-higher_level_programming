7f295eb49c9782814e9efd7dac7da65063c18dde
Elizabeth Agada <lizzyegbo2015@gmail.com>
2023-11-29 09:24:40 -0800

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,28 +3,31 @@
 /**
  * insert_node - program that inserts numbers into a sorted
  * singly-linked list.
- * @head: pointer to the head of slinged linked list
- * @number: The number to insert
- * Return: if the function fails - NULL
- * else - a pointer ro the new node.
+ * @head: pointer to the head of slinged linked list.
+ * @number: The number to insert.
+ * Author - ElizabethAgada
+ * Return: if the function fails - NULL.
+ *         otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *node = *head, new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
-
-	while (node && node->next && node->next->n < number)
+{       
+        listint_t *node = *head, *new;
+        
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = number;
+        
+        if (node == NULL || node->n >= number)
+        {       
+                new->next = node;
+                *head = new;
+                return (new);
+        }       
+        
+        while (node && node->next && node->next->n < number)
 		node = node->next;
+
 	new->next = node->next;
 	node->next = new;
 
