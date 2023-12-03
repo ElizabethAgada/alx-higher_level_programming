2f47d1ebcf3e4cd1abf4faf52d3034f345b54fb3
Elizabeth Agada <lizzyegbo2015@gmail.com>
2023-12-03 14:55:50 -0800

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
