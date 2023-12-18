9df3bd044faa64a25baed82f976eb2d13a7fbf06
Elizabeth Agada <lizzyegbo2015@gmail.com>
2023-12-18 15:48:07 -0800

    the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     prayer = 0
     for i in range(1, 3):
@@ -9,6 +10,6 @@ def magic_calculation(a, b):
             else:
                 prayer += a ** b / i
         except Exception:
-            result = b + a
+            prayer = b + a
             break
-        return (result)
+    return (prayer)
