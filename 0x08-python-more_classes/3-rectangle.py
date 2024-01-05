fa62c854edf2fa631e61c1ffb7bdf216b813be09
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-01-05 07:15:49 -0800

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
 
         Args:
             width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle."""
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
@@ -49,13 +49,12 @@ class Rectangle:
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return ((self.__width * 2) + (self.__height ** 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
         Represents the rectangle with the  # character.
-
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
