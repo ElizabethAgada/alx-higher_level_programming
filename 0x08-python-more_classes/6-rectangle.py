ef9e8b6e0305cc4d6b37bc1eeb4ce54baff14c3c
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-01-06 09:15:52 -0800

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -44,7 +44,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value = 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -61,15 +61,15 @@ class Rectangle:
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
-        Represents the rectangle with the  # character.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
-        for h in range(self.__height):
-            [rect.append('#') for z in range(self.__width)]
-            if h != self.__height - 1:
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
