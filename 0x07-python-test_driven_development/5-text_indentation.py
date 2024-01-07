858c7b5d22637e6f032a9df2c1e5a77865f57356
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-01-07 14:13:47 -0800

    a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    If not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    h = 0
+    while h < len(text) and text[h] == ' ':
+        h += 1
+
+    while h < len(text):
+        print(text[h], end="")
+        if text[h] == "\n" or text[h] in ".?:":
+            if text[h] in ".?:":
+                print("\n")
+            h += 1
+            while h < len(text) and text[h] == ' ':
+                h += 1
+            continue
+        h += 1
