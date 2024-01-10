c6ed361b382ca0a495b97633961ac5268f15b111
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-01-09 16:44:19 -0800

    a class Student that defines a student by: based on 9-student.py Public instance attributes:, first_name, last_name^C

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defining a class Student."""
+
+
+class Student:
+    """Representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a new Student.
+
+        Args:
+            first_name (str): First name of student.
+            last_name (str): Last name of student.
+            age (int): Age of student.
+        """
+        self.first_name - first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get/set a dictionary representation of Student.
+
+        if attrs is a list of strings, represent only the attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) Attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return (z: getattr(self, z) for z in attrs if hasattr(self, z))
+        return self.__dict__
