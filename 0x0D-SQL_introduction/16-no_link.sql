e08bbfd8904475b8e56adab4cef33ef9f8edfe83
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-02-15 17:32:15 -0800

    SQL INTRO

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
--- Lists all d records of d table second_table having a name value in my MySQL server.
-SELECT `score`, `name'
+-- Lists all records of the table second_table having a name value in my MySQL server.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
-ORDER BY `score` DESC;
+ORDER BY `score` DESC
