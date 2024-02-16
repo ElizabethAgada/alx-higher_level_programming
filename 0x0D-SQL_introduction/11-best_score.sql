006db629f06dd89c5e40c43001d825104588a52a
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-02-15 17:19:16 -0800

    SQL INTRO

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 -- Lists all the records that are in d table second-table with a score >= 10 in My MySQL server.
 -- Records are ordered by descending score.
-SELECT `score` `name` FROM `second_table`
-WHERE `score` >=10
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
 ORDER BY `score` DESC;
