c04ba0600398f857491172900fce0f4a4076769f
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-02-19 09:26:18 -0800

    user permission

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- list all priviledges of MySQL users user_0d_1 and user_0d_2.
+-- lists all priviledges of MySQL users user_0d_1 and user_0d_2on my server in localhost
 SHOW GRANTS FOR `user_0d_1`@localhost`;
 SHOW GRANTS FOR `aser_0d_2`@localhost`;
