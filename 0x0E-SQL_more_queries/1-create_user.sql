088f95ee41a165ac1b1bc31a9b3ccf22f003c305
ElizabethAgada <lizzyegbo2015@gmail.com>
2024-02-20 09:54:41 +0000

    continue

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- craetes user, if the user exist, script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
