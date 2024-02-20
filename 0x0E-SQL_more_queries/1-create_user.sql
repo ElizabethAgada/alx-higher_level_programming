3b800921775abde50ce9e86188234018fc598a8b
ElizabethAgada <lizzyegbo2015@gmail.com>
2024-02-20 09:31:43 +0000

    continue

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creates user if it does not exist
+-- creates the MySQL server user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO  `user_0d_1'@'localhost' WITH GRANT OPTION;
