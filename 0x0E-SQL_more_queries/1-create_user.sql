51f2508e849b71688a72ce531090e7b462d673ed
ElizabethAgada <lizzyegbo2015@gmail.com>
2024-02-20 09:39:06 +0000

    continue

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creates the MySQL server user user_0d_1 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO  `user_0d_1'@'localhost' WITH GRANT OPTION;
+-- craetes user, if the user exist, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
