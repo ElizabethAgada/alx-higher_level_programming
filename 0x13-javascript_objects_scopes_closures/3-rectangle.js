2567163b275ba7542c8ac798fb06a58dd4a42763
ElizabethAgada <lizzyegbo2015@gmail.com>
2024-03-12 06:31:29 -0700

    java class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let breeze = 0; breeze < this.height; breeze++) {
+      let love = '';
+      for (let break = 0; break < this.width; break++) {
+        love += 'X';
+      }
+	    console.log(love);
+    }
+  }
+}
+
+module.exports = Rectangle;
