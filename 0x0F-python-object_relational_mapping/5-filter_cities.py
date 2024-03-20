3083c7cfec23be136e475110b1e8720768d65737
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-20 13:11:31 -0700

    done2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this lists all cities & states 4rom d database hbtn_0e_0_usa
+lists  all cities and states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     match = sys.argv[4]
     cur.execute("""SELECT cities.name FROM
