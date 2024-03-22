e779922658cd402869fb1697ba48acd65a0f7cbc
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-22 15:30:40 -0700

    task 13 and 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This lists states with ascending order of state ids"""
+"""lists states with ascending order of state ids"""
 
 
 import sys
@@ -11,10 +11,10 @@ from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3])
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in (session.query(State.name, City.id, City.name)
-                .filter(State.id == City.state_id):
-    print(instance[0] + ": (" + str(instance[1] + ") " + instance[2])
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]
