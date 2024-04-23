02f29547019a9af3f4b97005c7476eced6237421
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-04-23 15:50:36 -0700

    javascript web scrapping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,16 +4,16 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    princeCharacters(characters, 0);
+    printCharacters(characters, 0);
   }
 });
 
 function printCharacters (characters, index) {
-  request (characters[index], function (error, response, body) {
+  request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
-	printCharacters(characters, index + 1);
+        printCharacters(characters, index + 1);
       }
     }
   });
