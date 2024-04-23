3f214bb2490647f4b952f87747f3cd3b6384c281
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-04-23 16:00:32 -0700

    javascript web scrapping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL ='https://swapi-api.hbtn.io/api/films/';
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
-   console.log(err);
+    console.log(err);
   } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
